1775785f7932814103b4d068c507129c44c3f284
root <zakariarassili@gmail.com>
2023-10-20 17:49:19 +0000

    update pycodestyle

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,9 +4,8 @@
 The application listens on 0.0.0.0, port 5000.
 """
 from flask import Flask
-
-app = Flask(__name__)
 from flask import abort
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
