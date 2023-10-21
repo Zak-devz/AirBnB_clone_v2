7e8fc3ca6162fca229bee77b26fa349661d7e6f7
Zak-devz <zakariarassili@gmail.com>
2023-10-22 00:21:47 +0100

    update list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,29 +2,67 @@
 """Starts a Flask web application.
 
 The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
 """
 from models import storage
+from models.state import State
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
+@app.route("/", strict_slashes=False)
+def hello():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
-    States are sorted by name.
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
     """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Displays 'n is a number' only if n is an integer."""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Displays an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Displays an HTML page with a list of all State objects in DBStorage """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def close(error):
+    """ Remove the current SQLAlchemy session """
     storage.close()
 
 
