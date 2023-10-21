a63517cf746ee9cf492bf27ace1e0a0b00863654
Zak-devz <zakariarassili@gmail.com>
2023-10-22 00:28:40 +0100

    task 9  dcompleted

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+"""
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Displays 'n is a number' only if n is an integer."""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Displays an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Displays an HTML page with a list of all State objects in DBStorage """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close(error):
+    """ Remove the current SQLAlchemy session """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Displays an HTML page with a list of all states and related cities """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
